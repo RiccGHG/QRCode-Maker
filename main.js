c49874491cc871833a4dfed8cb2059c064720520
RiccGHG <richard10hahn@gmail.com>
2026-01-09 21:37:06 +0000

    Added text area and made it beutiful

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 function gen() {
-    QRCode.toDataURL("hi", (err, url) => {
-        if (err) return console.error(err);
-        document.getElementById("img").src = url
-    })
+  const input = document.querySelector("textarea");
+  if (!input.value) {
+    input.style.outline = "2px solid red";
+    setTimeout(() => {
+      input.style.outline = "";
+    }, 1000 * 3);
+    return;
+  }
+  QRCode.toDataURL(input.value, (err, url) => {
+    if (err) return console.error(err);
+    let img = document.getElementById("img");
+    img.src = url;
+    img.hidden = false;
+    const downloadBtn = document.getElementById("downloadBtn");
+    downloadBtn.disabled = false;
+    const downloader = document.getElementById("download");
+    downloader.href = url;
+  });
+}
+function download() {
+    const download = document.getElementById("download");
+    download.click();
+    console.log("click");
+    
 }
